8a3a766e71de17fb507c4dabcf578231f061a15f
TB52052 <taylorbullard04@outlook.com>
2023-05-12 10:58:15 +0100

    Initial Commit
    No tests implemented yet

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println(Calculator.addition(1,2));
+        System.out.println(Calculator.subtraction(1,2));
+        System.out.println(Calculator.multiplication(1,2));
+        System.out.println(Calculator.division(1,2));
+    }
+}
---
+++ src/test/java/testCalculator.java
@@ -0,0 +1,10 @@
+import org.example.Calculator;
+
+public class testCalculator {
+
+    public void testAddition(){
+        int result = Calculator.addition(1,2);
+
+    }
+
+}
